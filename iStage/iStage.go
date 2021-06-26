72f0b3bb235f27b1bfd40c82054d816308153fe9
Helmut Kemper <helmut.kemper@gmail.com>
2021-06-25 21:37:10 -0300

    working

--- iStage/iStage.go
+++ iStage/iStage.go
@@ -3,11 +3,16 @@ package iStage
 import (
 	iotmaker_santa_isabel_theater_interfaces "github.com/helmutkemper/iotmaker.santa_isabel_theater.interfaces"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.interfaces/iEngine"
+	iotmaker_platform_IDraw "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.IDraw"
 )
 
 type IStage interface {
 	AddEngine(engine iEngine.IEngine)
 	GetEngine() iEngine.IEngine
+	GetStage() IStage
+	GetCanvas() iotmaker_platform_IDraw.IDraw
+	GetScratchPad() iotmaker_platform_IDraw.IDraw
+	GetCache() iotmaker_platform_IDraw.IDraw
 	AddToIndexList(id string, index int, collision func(x, y int) bool)
 	RemoveFromIndexList(id string)
 	IsDraggable(x, y int) string
